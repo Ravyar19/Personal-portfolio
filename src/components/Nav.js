3c1ba00526fb83c989415dad360057151c6032dc
Your Name <captainravyar@gmail.com>
2021-07-30 14:14:51 +0300

    style:nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,78 +1,156 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
-import { Link, useHistory } from "react-router-dom"
-import { motion } from "framer-motion"
+import { FaTimes, FaBars } from "react-icons/fa"
+import { IconContext } from "react-icons/lib"
+import { Link } from "react-router-dom"
 
 const Nav = () => {
-  const history = useHistory()
-  const activePath = history.location.pathname
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+
   return (
-    <StyledNav>
-      <h1>
-        <Link id="logo" to="/">
-          Ravyar
-        </Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to="/">1. About Us</Link>
-          <Line
-            transition={{ duration: 0.75 }}
-            initial={{ width: "0%" }}
-            animate={{ width: activePath === "/" ? "50%" : "0%" }}
-          />
-        </li>
-        <li>
-          <Link to="/work">2. Our Work</Link>
-          <Line
-            transition={{ duration: 0.75 }}
-            initial={{ width: "0%" }}
-            animate={{ width: activePath === "/work" ? "50%" : "0%" }}
-          />
-        </li>
-        <li>
-          <Link to="/contact">3. Contact Us</Link>
-          <div className="line3"></div>
-        </li>
-      </ul>
-    </StyledNav>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <NavM>
+          <NavbarContainer>
+            <NavLogo>
+              <h1>Ravyar</h1>
+            </NavLogo>
+
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+
+            <NavMenu click={click} onClick={handleClick}>
+              <NavItem>
+                <NavLinks to="/">
+                  <h4>1. About Me</h4>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/">
+                  <h4>2. My Work </h4>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/">
+                  <h4>3. Contact Me</h4>
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </NavM>
+      </IconContext.Provider>
+    </>
   )
 }
 
-const StyledNav = styled.nav`
-  min-height: 10vh;
+export default Nav
+
+const NavM = styled.nav`
+  background: #282828;
+  height: 80px;
   display: flex;
   margin: auto;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  background: #282828;
+  font-size: 1.2rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
   padding: 1rem 10rem;
-  ul {
+`
+
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  max-width: 1300px;
+  margin-right: 5rem;
+  margin-left: auto;
+
+  @media (max-width: 991px) {
+    padding-right: 300px;
+    padding-left: 30px;
+  }
+`
+const NavLogo = styled.div`
+  color: #fff;
+  justify-self: flex-start;
+  cursor: pointer;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  font-family: "Lobster", cursive;
+  font-weight: lighter;
+`
+const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1100px) {
+    display: block;
+    position: absolute;
+    top: 1.5rem;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+
+const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+
+  @media screen and (max-width: 1100px) {
     display: flex;
-    list-style: none;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: ${({ click }) => (click ? 0 : "-100%")};
+    opacity: 1;
+    transition: all 0.5 ease;
+    background: #101522;
   }
-  li {
-    padding-left: 10rem;
-    position: relative;
-  }
-  a {
-    color: white;
-    text-decoration: none;
-  }
-  #logo {
+`
+const NavItem = styled.li`
+  height: 80px;
+  padding: 2rem;
+  h4 {
+    font-weight: 200;
     font-size: 1.5rem;
-    font-family: "Lobster", cursive;
-    font-weight: lighter;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    padding-bottom: 5rem;
   }
 `
 
-const Line = styled(motion.div)`
-  height: 0.3rem;
-  background: #23d997;
-  width: 5%;
-  position: absolute;
-  bottom: -80%;
-  left: 60%;
-`
+const NavLinks = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  height: 100%;
 
-export default Nav
+  @media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+    &:hover {
+      color: red;
+      transition: all 0.3 ease;
+    }
+  }
+`
